4d72ebd754aa017e03987777595364f197e3c8b3
Bhagyshri1504 <102225187+Bhagyshri1504@users.noreply.github.com>
2022-03-23 21:43:53 +0530

    Create arrayList.java

---
+++ arrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class arrayList
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer>numbers=new ArrayList<Integer>();
+        numbers.add(22);
+        numbers.add(44);
+        numbers.add(6);
+        numbers.add(19);
+        numbers.add(30);
+        System.out.println(numbers);
+    }
+}
